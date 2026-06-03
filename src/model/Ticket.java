8b95e54fef69e0fb7692becc305c320b7282e084
seeuniiii <3epark@gmail.com>
2026-06-03 11:49:36 +0900

    Update Ticket.java

--- src/model/Ticket.java
+++ src/model/Ticket.java
@@ -22,14 +22,20 @@ public class Ticket {
 //시간권
 class TimeTicket extends Ticket {
     private int addHours;
+    private int addMinutes;
 
-    public TimeTicket(String ticketName, int price, int addHours) {
+    public TimeTicket(String ticketName, int price, int addHours, int addMinutes) {
         super(ticketName, price);
         this.addHours = addHours;
+        this.addMinutes = addMinutes;
     }
 
     public int getAddHours() {
         return addHours;
+    }
+
+    public int getAddMinutes() {
+        return addMinutes;
     }
 }
 
